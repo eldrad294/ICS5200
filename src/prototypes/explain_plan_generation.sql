8cb427d14879774bd5e754b32b72e0b0939a4792
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-07-12 09:45:21 +0200

    Added documentation + links

--- src/prototypes/explain_plan_generation.sql
+++ src/prototypes/explain_plan_generation.sql
@@ -1,5 +1,11 @@
+/*
+Documentation
+* https://dba.stackexchange.com/questions/84012/what-are-the-omem-and-1mem-columns-in-the-plan-output
+* http://www.dba-oracle.com/t_gather_plan_statistics.htm
+*/
+
 --explain plan for
-select /*+ gather_plan_statistics */*
+select /*+ gather_plan_statistics */ *
 from E_ACCOUNT_PERMISSION
 where ap_ca_id = 0001
 order by ap_acl desc;
