09b62e61d67e1cae0153ddca92458eb2bc5e3554
Gabriel <eldrad294@hotmail.com>
2018-07-16 17:59:54 +0200

    Minor Revision

--- src/main/main.py
+++ src/main/main.py
@@ -1,5 +1,11 @@
 #
 # Module Imports
+import sys
+from os.path import dirname, abspath
+print(dirname(dirname(abspath(__file__))))
+sys.path.append(dirname(dirname(abspath(__file__))))
+for p in sys.path:
+    print(p)
 print('Entry0')
 from src.utils.db_interface import db_conn
 #
