df8225cd761c3b03b07bfcf76bb70175592f8257
Gabriel Sammut <eldrad294@hotmail.com>
2018-07-23 09:44:34 +0200

    Bug Fix

--- src/data/loading.py
+++ src/data/loading.py
@@ -27,7 +27,7 @@ class FileLoader:
             raise Exception('Master was not declared for Spark context!')
     #
     def load_data(self, path, table_name, db_conn):
-        dist_file = self.sc.textFile(path)
+        dist_file = self.sc.textFile(path, mode="utf-8")
         l_dist_file = dist_file.collect() # Convert into python collection (list)
         for i, line in enumerate(l_dist_file):
             dml, bind_values = self.__build_insert(line, table_name)
@@ -75,9 +75,9 @@ class FileLoader:
                     except Exception:
                         pass
                 #
-                if type(value) == "str":
-                    # Required for data loading due to some illegal characters within TPC-DS
-                    value = value.encode('utf-8')
+                # if type(value) == "str":
+                #     # Required for data loading due to some illegal characters within TPC-DS
+                #     value = value.encode('utf-8')
                 #
                 list_line.append(value)
                 value = ""
