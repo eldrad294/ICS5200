3f84c889f8b0aeb0819f91cb2cc1e5db36ebbdc3
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-04 15:31:31 +0200

    Added partial functionality to Spark Handling

--- src/data/loading.py
+++ src/data/loading.py
@@ -54,7 +54,7 @@ class FileLoader:
     def load_data(self, path, table_name):
         rdd_file = self.sc.textFile(path, self.__rdd_parallelism) # Materializes an RDD, but does not compute due to lazy evaluation
         rdd_file = rdd_file.map(lambda x: x.split('\n')) # Split line by line - does not compute immediately due to lazy evaluation
-        rdd_file.foreachPartition(partial(SparkMaps.build_insert, arg1=table_name))
+        rdd_file.foreachPartition(partial(SparkMaps.build_insert, table_name=table_name))
         db_conn.commit()
         #
         logger.log("Loaded table [" + table_name + "] into database..")
