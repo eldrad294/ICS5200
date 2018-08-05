c674bbfe5a0ab519b32687c0539744fb3cc2c0bd
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-05 13:48:59 +0200

    Project Revamp - Code Refactoring

--- src/data/tpc.py
+++ src/data/tpc.py
@@ -218,9 +218,8 @@ class FileLoader:
         rdd_file = self.__spark_context.textFile(path, self.__ev_loader.var_get('spark_rdd_partitions')) # Materializes an RDD, but does not compute due to lazy evaluation
         mapped_rdd_file = rdd_file.map(lambda x: x.split('\n')) # Split line by line - does not compute immediately due to lazy evaluation
         db_conn = self.__database_context
-        f = lambda line: SparkMaps.build_insert(dataline=line,
-                                                table_name=table_name,
-                                                database_context=db_conn)
-        mapped_rdd_file.foreachRDD(lambda rdd: rdd.foreachPartition(f))
+        mapped_rdd_file.foreach(lambda line : SparkMaps.build_insert(dataline=line,
+                                                                     table_name=table_name,
+                                                                     database_context=db_conn))
         #
         self.__logger.log("Loaded table [" + table_name + "] into database..")
