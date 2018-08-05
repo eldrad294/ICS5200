d5a212c97e6df97cfb16d38e93366d48f8d24a58
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-05 11:03:38 +0200

    Project Revamp - Code Refactoring

--- src/data/spark_maps.py
+++ src/data/spark_maps.py
@@ -13,7 +13,7 @@ class SparkMaps:
         :param spark_context: Spark connection context
         :return:
         """
-        print(dataline)
+        print(dataline.value)
         l_line = SparkMaps.__parse_data_line(dataline=dataline)
         dml = "INSERT INTO " + table_name + " VALUES ("
         for i in range(len(l_line)):
