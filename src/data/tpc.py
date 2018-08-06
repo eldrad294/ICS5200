5f46de24867b1314da9c72a169344a44f81ea751
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-08-06 14:34:49 +0200

    RDD development

--- src/data/tpc.py
+++ src/data/tpc.py
@@ -237,6 +237,6 @@ class FileLoader:
                             self.__ev_loader.var_get('service'),
                             self.__ev_loader.var_get('port'),
                             self.__ev_loader.var_get('password')]
-        rdd_file.rdd.foreachPartition(lambda line: SparkMaps.send_partition(data=line,
+        rdd_file.foreachPartition(lambda line: SparkMaps.send_partition(data=line,
                                                                 table_name=table_name,
                                                                 instance_details=instance_details))
