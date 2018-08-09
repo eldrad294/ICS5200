8ac7913d88aa9c81f89f0b8601444757c1740e55
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-08-09 11:43:10 +0200

    Added tablespace handling

--- src/main/setup.py
+++ src/main/setup.py
@@ -68,7 +68,7 @@ if ev_loader.var_get('tpcds_data_loading_bool') == 'True':
     sql_statement = "select count(*) from user_tables where table_name = 'DBGEN_VERSION'"
     result = int(db_conn.execute_query(sql_statement, fetch_single=True)[0])
     if result < 1:
-        db_conn.executeScriptsFromFile(ev_loader.var_get("src_dir") + "/sql/Installation/tpcds_schema_tables.sql")
+        db_conn.executeScriptsFromFile(ev_loader.var_get("src_dir") + "/sql/Installation/schema_tables_" + ev_loader.var_get['user'] + ".sql")
         logger.log('TPC-DS table generation successful!')
     else:
         logger.log('Skipping schema creation..TPC-DS tables already exist!')
@@ -94,7 +94,7 @@ if ev_loader.var_get('tpcds_data_loading_bool') == 'True':
     result = int(db_conn.execute_query(sql_statement, fetch_single=True)[0])
     if result < 1:
         logger.log('Starting schema indexes creation..')
-        db_conn.executeScriptsFromFile(ev_loader.var_get("src_dir") + "/sql/Installation/tpcds_schema_indexes.sql")
+        db_conn.executeScriptsFromFile(ev_loader.var_get("src_dir") + "/sql/Installation/schema_indexes_" + ev_loader.var_get['user'] + ".sql")
         logger.log('TPC-DS indexes generation successful!')
     else:
         logger.log('Skipping schema creation..TPC-DS indexes already exist!')
---
+++ src/sql/Installation/create_tablespaces.sql
@@ -0,0 +1,22 @@
+/*
+Create Tablespaces
+
+* Tablespace | Datafiles can be checked from:
+select * from dba_tablespaces;
+select * from DBA_DATA_FILES;
+
+NB: Ensure that physical datafiles are deleted before running this script:
+1) CISK> cd /mnt/raid5/oradata/gabsam/
+2) CISK> sudo rm -f tpcds*
+*/
+CREATE TABLESPACE tpcds1   DATAFILE '/mnt/raid5/oradata/gabsam/tpcds1_01.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+CREATE TABLESPACE tpcds10  DATAFILE '/mnt/raid5/oradata/gabsam/tpcds10_01.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+CREATE TABLESPACE tpcds50  DATAFILE '/mnt/raid5/oradata/gabsam/tpcds50_01.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+CREATE TABLESPACE tpcds100 DATAFILE '/mnt/raid5/oradata/gabsam/tpcds100_01.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+/*
+Create datafiles
+*/
+ALTER TABLESPACE tpcds1 ADD DATAFILE '/mnt/raid5/oradata/gabsam/tpcds1_02.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+ALTER TABLESPACE tpcds1 ADD DATAFILE '/mnt/raid5/oradata/gabsam/tpcds10_02.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+ALTER TABLESPACE tpcds1 ADD DATAFILE '/mnt/raid5/oradata/gabsam/tpcds50_02.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
+ALTER TABLESPACE tpcds1 ADD DATAFILE '/mnt/raid5/oradata/gabsam/tpcds100_02.dbf' SIZE 5M AUTOEXTEND ON NEXT 1024K MAXSIZE UNLIMITED;
