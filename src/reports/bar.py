5343cc08771b34cba9d7ec5f98e5e0361b30f62f
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-11 16:41:06 +0200

    Added initial report handling

--- src/reports/bar.py
+++ src/reports/bar.py
@@ -16,7 +16,7 @@ class BarCharts:
         """
         self.__logger.log('Starting generation of report..')
         #
-        cur, schema = self.__db_conn.execute_query(query="select * from REP_TPC_DESCRIBE where tpc_type='" + tpc_type.upper() + "'",
+        cur, schema = self.__db_conn.execute_query(query="select * from REP_TPC_DESCRIBE where tpctype='" + tpc_type.upper() + "'",
                                                    describe=True)
         #
         print(schema)
