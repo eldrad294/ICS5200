d59076e7be3938c9f70452b55cbf52b4f1fa98b9
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-11 16:19:33 +0200

    Added initial report handling

--- src/reports/bar.py
+++ src/reports/bar.py
@@ -5,17 +5,19 @@ import src.reports.report_extraction_sql as res
 #
 class BarCharts:
     #
-    def __init__(self, db_conn):
+    def __init__(self, db_conn, logger):
         self.__db_conn = db_conn
+        self.__logger = logger
     #
     def generate_REP_TPC_DESCRIBE(self):
         """
         Generates the REP_TPC_DESCRIBE.sql reprot
         :return:
         """
-        logger.log('Starting generation of report..')
+        self.__logger.log('Starting generation of report..')
         #
         cur, schema = self.__db_conn.execute_query(res.REP_TPC_DESCRIBE, describe=True)
         #
         print(schema)
         print(cur)
+        self.__logger.log('Report generation complete')
