e63786cdc7775d1734e00e3dae81a0dea2eb6bce
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-11 15:18:40 +0200

    Added initial report handling

--- src/main/report.py
+++ src/main/report.py
@@ -1,17 +1,42 @@
-from src.reports.bar import BarCharts
-from src.framework.db_interface import ConnectionPool
-connection_details = {'instance_name':'gabsam',
-                              'user':'tpcds1',
-                              'host':'192.168.202.222',
-                              'service':'gabsam',
-                              'port':'1521',
-                              'password':'tpc'}
-logger = Logger(log_file_path=None,
-                     write_to_disk='false',
-                     write_to_screen=ev_loader.var_get('write_to_screen'))
-ConnectionPool.create_connection_pool(max_connections=1,
-                                      connection_details=connection_details,
-                                      logger=logger)
+"""
+--------------------------
+This script is used to generate reports
+The script will:
+* TBD
+--------------------------
+"""
+"""
+---------------------------------------------------
+SCRIPT WARM UP - Module Import & Path Configuration
+---------------------------------------------------
+"""
 #
+# Module Imports
+import sys
+from os.path import dirname, abspath
+#
+# Retrieving relative paths for project directory
+home_dir = dirname(dirname(dirname(dirname(abspath(__file__)))))
+project_dir = dirname(dirname(dirname(abspath(__file__))))
+src_dir = dirname(dirname(abspath(__file__)))
+#
+# Appending to python path
+sys.path.append(home_dir)
+sys.path.append(project_dir)
+sys.path.append(src_dir)
+#
+from src.framework.script_initializer import ScriptInitializer
+from src.framework.db_interface import ConnectionPool
+si = ScriptInitializer(project_dir=project_dir, src_dir=src_dir, home_dir=home_dir)
+ev_loader = si.get_global_config()
+db_conn = ConnectionPool.claim_from_pool()[2]
+spark_context = si.initialize_spark().get_spark_context()
+logger = si.initialize_logger()
+#
+"""
+----------------------------------
+SCRIPT EXECUTION - Report Generation
+----------------------------------
+"""
 bc = BarCharts(ConnectionPool.claim_from_pool())
 bc.generate_REP_TPC_DESCRIBE()
