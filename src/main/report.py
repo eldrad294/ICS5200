b21678622e57353c301a160bea9f2c7f1b6dc0c0
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-08-11 15:06:22 +0200

    Added REP_TPC_DESCRIBE SQL

--- src/main/report.py
+++ src/main/report.py
@@ -1,4 +1,4 @@
-from src.reports.bar import BarChart
+from src.reports.bar import BarCharts
 from src.framework.db_interface import ConnectionPool
 connection_details = {'instance_name':'gabsam',
                               'user':'tpcds1',
@@ -8,7 +8,7 @@ connection_details = {'instance_name':'gabsam',
                               'password':'tpc'}
 ConnectionPool.create_connection_pool(max_connections=1,
                                       connection_details=connection_details,
-                                      logger=self.logger)
+                                      logger=logger)
 #
-bc = BarChart(ConnectionPool.claim_from_pool())
+bc = BarCharts(ConnectionPool.claim_from_pool())
 bc.generate_REP_TPC_DESCRIBE()
