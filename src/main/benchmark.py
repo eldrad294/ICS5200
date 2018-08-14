ffd8d2072b8bebc4dcc0bb9e5b0693f94df63444
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-08-14 22:31:22 +0200

    Changed way how to split query_0.sql

--- src/main/benchmark.py
+++ src/main/benchmark.py
@@ -77,9 +77,7 @@ for i in range(1, ev_loader.var_get('iterations') + 1):
         with open(query_path + filename) as file:
             logger.log('Generating execution metrics for [' + filename + ']..')
             data = file.read()
-            sql_list = data.split(';')
-            for sql in sql_list:
-                xp.generateExecutionPlan(sql=sql, binds=None, selection=None, save_to_disk=True)
+            xp.generateExecutionPlan(sql=data, binds=None, selection=None, save_to_disk=True)
     # Execute All DML
     for filename in os.listdir(dml_path):
         with open(dml_path + filename) as file:
@@ -107,9 +105,7 @@ for i in range(1, ev_loader.var_get('iterations')+1):
         with open(query_path + filename) as file:
             logger.log('Generating execution metrics for [' + filename + ']..')
             data = file.read()
-            sql_list = data.split(';')
-            for sql in sql_list:
-                xp.generateExecutionPlan(sql=data, binds=None, selection=None, save_to_disk=True)
+            xp.generateExecutionPlan(sql=data, binds=None, selection=None, save_to_disk=True)
     # Execute All DML
     for filename in os.listdir(dml_path):
         with open(dml_path + filename) as file:
