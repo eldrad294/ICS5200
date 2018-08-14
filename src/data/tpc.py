22bfbf2550e1a8421c5247fdaf04eda739ae3b40
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-08-14 13:31:31 +0200

    Testing TPC Benchmark Script

--- src/data/tpc.py
+++ src/data/tpc.py
@@ -221,10 +221,11 @@ class TPC_Wrapper:
     #
     def __convert_cast_to_date(self, sql):
         sql = sql.replace(" days)","),'yyyy-mm-dd'")
-        sql = sql.replace("as date)",",'yyyy/mm/dd')")
+        sql = sql.replace("as date) +",",'yyyy/mm/dd')")
+        sql = sql.replace("as date)", ",'yyyy/mm/dd'),'yyyy-mm-dd'")
+        sql = sql.replace("(cast('", "to_char((to_date('")
         sql = sql.replace("cast('","to_char((to_date('")
         return sql
-
     #
     def __rename(self, oldfilename):
         """
