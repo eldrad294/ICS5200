5c7be706ba786d3124fcb0c7866d1be248c2debe
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-08-16 12:49:37 +0200

    Changed config

--- data/TPC-DS/tests/29_dml.sql
+++ data/TPC-DS/tests/29_dml.sql
@@ -1,5 +1,6 @@
 drop view cadrv;
-create /*ICS5200_MONITOR_HINT*/ view cadrv as
+drop table cadrv;
+create view cadrv as
 select cust_customer_id ,cust_street_number ,concat(cust_street_name1,cust_street_name2) street ,cust_street_type ,cust_suite_number ,cust_city ,cust_county ,cust_state ,cust_zip ,cust_country
 from s_customer_m;
 select count(*) from s_customer_m;
