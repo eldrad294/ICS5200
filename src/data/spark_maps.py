8729ead7f363fe3bb89f32794ddee3fe687ac0a3
Gabriel Sammut <eldrad294@hotmail.com>
2018-08-19 16:07:52 +0200

    Fixed bug with ordering of column names

--- src/data/spark_maps.py
+++ src/data/spark_maps.py
@@ -59,7 +59,7 @@ class LoadTPCData:
         dml = "INSERT INTO " + table_name + " " + column_names + " VALUES ("
         for count, data_line in enumerate(data):
             l_line = LoadTPCData.__parse_data_line(dataline=data_line)
-            if table_name == 'TIME_DIM' or table_name == 'time_dim':
+            if table_name == 'INVENTORY' or table_name == 'inventory':
                 logger.log(dml)
                 logger.log(l_line)
             if count < 1:
