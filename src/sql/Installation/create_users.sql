e0ad67be1ebad08654eb584a058bb2930f49dc0b
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-09-02 15:25:15 +0200

    Added extra permission handling

--- src/sql/Installation/create_users.sql
+++ src/sql/Installation/create_users.sql
@@ -11,6 +11,8 @@ grant execute on dbms_lock to tpcds1;
 grant execute on kill_long_running to tpcds1;
 grant alter system to tpcds1;
 GRANT SELECT ON V_$SESSION TO tpcds1;
+GRANT CREATE SESSION TO tpcds1;
+GRANT ALL PRIVILEGES TO tpcds1;
 --
 -- tpcds10
 create user tpcds10 identified by tpc;
@@ -21,16 +23,8 @@ grant execute on dbms_lock to tpcds10;
 grant execute on kill_long_running to tpcds10;
 grant alter system to tpcds10;
 GRANT SELECT ON V_$SESSION TO tpcds10;
---
--- tpcds50
-create user tpcds50 identified by tpc;
-grant connect to tpcds50;
-grant connect, resource, dba to tpcds50;
-grant unlimited tablespace to tpcds50;
-grant execute on dbms_lock to tpcds50;
-grant execute on kill_long_running to tpcds50;
-grant alter system to tpcds50;
-GRANT SELECT ON V_$SESSION TO tpcds50;
+GRANT CREATE SESSION TO tpcds10;
+GRANT ALL PRIVILEGES TO tpcds10;
 --
 -- tpcds100
 create user tpcds100 identified by tpc;
@@ -41,4 +35,6 @@ grant execute on dbms_lock to tpcds100;
 grant execute on kill_long_running to tpcds100;
 grant alter system to tpcds100;
 GRANT SELECT ON V_$SESSION TO tpcds100;
+GRANT CREATE SESSION TO tpcds100;
+GRANT ALL PRIVILEGES TO tpcds100;
 --
