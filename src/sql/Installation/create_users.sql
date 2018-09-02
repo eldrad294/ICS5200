6a21d681a4b4de58b57d05c3bd28e196ba474bea
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-09-02 11:49:46 +0200

    Added extra permissions

--- src/sql/Installation/create_users.sql
+++ src/sql/Installation/create_users.sql
@@ -7,21 +7,34 @@ create user tpcds1 identified by tpc;
 grant connect to tpcds1;
 grant connect, resource, dba to tpcds1;
 grant unlimited tablespace to tpcds1;
+grant execute on dbms_lock to tpcds1;
+grant alter system to tpcds1;
+GRANT SELECT ON V_$SESSION TO tpcds1;
 --
 -- tpcds10
 create user tpcds10 identified by tpc;
 grant connect to tpcds10;
 grant connect, resource, dba to tpcds10;
 grant unlimited tablespace to tpcds10;
+grant execute on dbms_lock to tpcds10;
+grant alter system to tpcds10;
+GRANT SELECT ON V_$SESSION TO tpcds10;
 --
 -- tpcds50
 create user tpcds50 identified by tpc;
 grant connect to tpcds50;
 grant connect, resource, dba to tpcds50;
 grant unlimited tablespace to tpcds50;
+grant execute on dbms_lock to tpcds50;
+grant alter system to tpcds50;
+GRANT SELECT ON V_$SESSION TO tpcds50;
 --
 -- tpcds100
 create user tpcds100 identified by tpc;
 grant connect to tpcds100;
 grant connect, resource, dba to tpcds100;
 grant unlimited tablespace to tpcds100;
+grant execute on dbms_lock to tpcds100;
+grant alter system to tpcds100;
+GRANT SELECT ON V_$SESSION TO tpcds100;
+--
--- src/sql/Utility/kill_long_running_jobs.sql
+++ src/sql/Utility/kill_long_running_jobs.sql
@@ -1,24 +1,30 @@
-declare
-  sid number;
-  serial number;
+create or replace procedure kill_long_running
+  (i_secs in number := 60)
+as
 begin
+  while (true)
   loop
-	begin
-	  select sid, serial#
-	  into sid, serial
-	  from v$session
-	  where program like 'python3%'
-	  and schemaname like 'TPC%'
-	  and username like 'TPC%'
-	  and status = 'ACTIVE'
-	  and last_call_et > 60;
-	  dbms_system.set_ev(sid, serial, 10237, 1, '');
-	  dbms_output.put_line('Terminated SID ['||sid||'] SERIAL ['||serial||']');
-	  dbms_lock.sleep(10);
-	  dbms_system.set_ev(sid, serial, 10237, 0, '');
+    --
+    begin
+      for rec in (select 'alter system kill session '''||sid||','||serial#||'''' as dml
+					from v$session
+					where username like 'TPC%'
+					and status = 'ACTIVE'
+					and program like '%python%'
+					and sysdate - interval ''||i_secs||'' minute > logon_time)
+      loop
+        begin
+        execute immediate rec.dml;
+        exception
+          when others then
+            null;
+        end;
+      end loop;
     exception
       when no_data_found then
-        dbms_lock.sleep(10);
+        null;
     end;
+    --
+    dbms_lock.sleep(10);
   end loop;
 end;
