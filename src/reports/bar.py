a5a1aba3d4e12bd1192448c8c8e6e78446869a17
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-09-10 19:57:42 +0200

    Ammending report for benchmark stat generation

--- src/reports/bar.py
+++ src/reports/bar.py
@@ -145,14 +145,14 @@ class BarCharts:
         #
         print(schema)
         print(cur)
-        print(list(zip(cur))[0])
+        print(cur[0][:])
         for col in columns:
             for i in range(len(schema)):
                 if col == schema[i]:
                     data = Data([
                         Bar(
                             x=transaction_bank,
-                            y=list(zip(cur))[i],
+                            y=cur[i][:],
                             name=col # Bar Title
                         )
                     ])
