ded570bc16cb1d0a9a4ac60326b70ea5dcc31100
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-09-15 15:10:05 +0200

    Enhanced schema

--- src/sql/Reports/REP_VSQL_PLAN.sql
+++ src/sql/Reports/REP_VSQL_PLAN.sql
@@ -1,7 +1,11 @@
 create table REP_VSQL_PLAN as
 select *
 from v$sql_plan
-where sql_id in (
+and 1=0;
+--
+select *
+from v$sql_plan vsp
+where vsp.sql_id in (
 	select dhsql.sql_id
 	from dba_hist_sqlstat dhsql,
 	     dba_hist_snapshot dhsnap
@@ -10,17 +14,10 @@ where sql_id in (
 	and dhsql.instance_number = dhsnap.instance_number
 	and dhsnap.snap_id between '544' and '545'
 )
-and 1=0;
---
-select *
-from v$sql_plan
-where sql_id in (
-	select dhsql.sql_id
-	from dba_hist_sqlstat dhsql,
-	     dba_hist_snapshot dhsnap
-	where dhsql.snap_id = dhsnap.snap_id
-	and dhsql.dbid = dhsnap.dbid
-	and dhsql.instance_number = dhsnap.instance_number
-	and dhsnap.snap_id between '544' and '545'
-);
+and vsp.timestamp = (
+  select max(timestamp)
+  from v$sql_plan
+  where sql_id = vsp.sql_id
+)
+order by sql_id, id;
 --
