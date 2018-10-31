7c7dd8fd58f418a7b26b5651fb0c1bb52964bbaf
Gabriel Sammut <eldrad294@hotmail.com>
2018-10-31 19:45:15 +0100

    Regression Bug Fix

--- src/data/workload.py
+++ src/data/workload.py
@@ -368,7 +368,7 @@ class Workload:
                     '       "projection", ' \
                     '       "time", ' \
                     '       "qblock_name", ' \
-                    '       "timestamp" from dual'
+                    '       "time_stamp" from dual'
             logger.log(query)
         elif report_type == 'rep_hist_sysmetric_summary':
             query = "select column_name " \
