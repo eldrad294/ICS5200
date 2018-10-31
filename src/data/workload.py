105b0df01216db2332bc41644641d42429ce2663
Gabriel Sammut <eldrad294@hotmail.com>
2018-10-31 19:51:46 +0100

    Regression Bug Fix

--- src/data/workload.py
+++ src/data/workload.py
@@ -336,39 +336,39 @@ class Workload:
             #         "from all_tab_columns " \
             #         "where table_name = 'DBA_HIST_SQL_PLAN' " \
             #         "order by column_id"
-            query = 'select "sql_text", ' \
-                    '       "sql_id", ' \
-                    '       "plan_hash_value", ' \
-                    '       "id", ' \
-                    '       "operation", ' \
-                    '       "options", ' \
-                    '       "object_node", ' \
-                    '       "object#", ' \
-                    '       "object_owner", ' \
-                    '       "object_name", ' \
-                    '       "object_alias", ' \
-                    '       "object_type", ' \
-                    '       "optimizer", ' \
-                    '       "parent_id", ' \
-                    '       "depth", ' \
-                    '       "position", ' \
-                    '       "search_columns", ' \
-                    '       "cost", ' \
-                    '       "cardinality", ' \
-                    '       "bytes", ' \
-                    '       "partition_start", ' \
-                    '       "partition_stop", ' \
-                    '       "partition_id", ' \
-                    '       "distribution", ' \
-                    '       "cpu_cost", ' \
-                    '       "io_cost", ' \
-                    '       "temp_space", ' \
-                    '       "access_predicates", ' \
-                    '       "filter_predicates", ' \
-                    '       "projection", ' \
-                    '       "time", ' \
-                    '       "qblock_name", ' \
-                    '       "time_stamp" from dual'
+            query = "select 'sql_text', " \
+                    "       'sql_id', " \
+                    "       'plan_hash_value', " \
+                    "       'id', " \
+                    "       'operation', " \
+                    "       'options', " \
+                    "       'object_node', " \
+                    "       'object#', " \
+                    "       'object_owner', " \
+                    "       'object_name', " \
+                    "       'object_alias', " \
+                    "       'object_type', " \
+                    "       'optimizer', " \
+                    "       'parent_id', " \
+                    "       'depth', " \
+                    "       'position', " \
+                    "       'search_columns', " \
+                    "       'cost', " \
+                    "       'cardinality', " \
+                    "       'bytes', " \
+                    "       'partition_start', " \
+                    "       'partition_stop', " \
+                    "       'partition_id', " \
+                    "       'distribution', " \
+                    "       'cpu_cost', " \
+                    "       'io_cost', " \
+                    "       'temp_space', " \
+                    "       'access_predicates', " \
+                    "       'filter_predicates', " \
+                    "       'projection', " \
+                    "       'time', " \
+                    "       'qblock_name', " \
+                    "       'timestamp' from dual"
             logger.log(query)
         elif report_type == 'rep_hist_sysmetric_summary':
             query = "select column_name " \
