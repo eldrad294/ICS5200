65a75d9a8f5d48462ea72c6e320bfe087e40f7da
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-11-01 15:45:38 +0100

    further rep-vsql

--- src/data/workload.py
+++ src/data/workload.py
@@ -336,39 +336,39 @@ class Workload:
             #         "from all_tab_columns " \
             #         "where table_name = 'DBA_HIST_SQL_PLAN' " \
             #         "order by column_id"
-            query = "select 'SQL_TEXT', " \
-                    "       'SQL_ID', " \
-                    "       'PLAN_HASH_VALUE', " \
-                    "       'ID', " \
-                    "       'OPERATION', " \
-                    "       'OPTIONS', " \
-                    "       'OBJECT_NODE', " \
-                    "       'OBJECT#', " \
-                    "       'OBJECT_OWNER', " \
-                    "       'OBJECT_NAME', " \
-                    "       'OBJECT_ALIAS', " \
-                    "       'OBJECT_TYPE', " \
-                    "       'OPTIMIZER', " \
-                    "       'PARENT_ID', " \
-                    "       'DEPTH', " \
-                    "       'POSITION', " \
-                    "       'SEARCH_COLUMNS', " \
-                    "       'COST', " \
-                    "       'CARDINALITY', " \
-                    "       'BYTES', " \
-                    "       'PARTITION_START', " \
-                    "       'PARTITION_STOP', " \
-                    "       'PARTITION_ID', " \
-                    "       'DISTRIBUTION', " \
-                    "       'CPU_COST', " \
-                    "       'IO_COST', " \
-                    "       'TEMP_SPACE', " \
-                    "       'ACCESS_PREDICATES', " \
-                    "       'FILTER_PREDICATES', " \
-                    "       'PROJECTION', " \
-                    "       'TIME', " \
-                    "       'QBLOCK_NAME', " \
-                    "       'TIMESTAMP' from dual"
+            query = "select 'SQL_TEXT'," \
+                    "'SQL_ID'," \
+                    "'PLAN_HASH_VALUE'," \
+                    "'ID'," \
+                    "'OPERATION'," \
+                    "'OPTIONS'," \
+                    "'OBJECT_NODE'," \
+                    "'OBJECT#'," \
+                    "'OBJECT_OWNER'," \
+                    "'OBJECT_NAME'," \
+                    "'OBJECT_ALIAS'," \
+                    "'OBJECT_TYPE'," \
+                    "'OPTIMIZER'," \
+                    "'PARENT_ID'," \
+                    "'DEPTH'," \
+                    "'POSITION'," \
+                    "'SEARCH_COLUMNS'," \
+                    "'COST'," \
+                    "'CARDINALITY'," \
+                    "'BYTES'," \
+                    "'PARTITION_START'," \
+                    "'PARTITION_STOP'," \
+                    "'PARTITION_ID'," \
+                    "'DISTRIBUTION'," \
+                    "'CPU_COST'," \
+                    "'IO_COST'," \
+                    "'TEMP_SPACE'," \
+                    "'ACCESS_PREDICATES'," \
+                    "'FILTER_PREDICATES'," \
+                    "'PROJECTION'," \
+                    "'TIME'," \
+                    "'QBLOCK_NAME'," \
+                    "'TIMESTAMP' from dual"
         elif report_type == 'rep_hist_sysmetric_summary':
             query = "select column_name " \
                     "from ( " \
