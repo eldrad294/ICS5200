374687c91f091c0b6bbc782601e23a6a52939f4a
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-12-24 10:37:32 +0100

    Minor addition

--- src/sql/Runtime/TPC-DS/tpcds100/Query/query_1.sql
+++ src/sql/Runtime/TPC-DS/tpcds100/Query/query_1.sql
@@ -1,26 +1,28 @@
-WITH customer_total_return 
-     AS (SELECT sr_customer_sk AS ctr_customer_sk, 
-                sr_store_sk    AS ctr_store_sk, 
-                Sum(sr_fee)    AS ctr_total_return 
-         FROM   store_returns, 
-                date_dim 
-         WHERE  sr_returned_date_sk = d_date_sk 
+WITH customer_total_return
+     AS (SELECT sr_customer_sk AS ctr_customer_sk,
+                sr_store_sk    AS ctr_store_sk,
+                Sum(sr_fee)    AS ctr_total_return
+         FROM   store_returns,
+                date_dim
+         WHERE  sr_returned_date_sk = d_date_sk
                 AND d_year = 2000
+                and sr_item_sk = 99700
+                and sr_ticket_number = 140324
                 AND rownum <= 10000
-         GROUP  BY sr_customer_sk, 
-                   sr_store_sk) 
-SELECT * 
-FROM   (SELECT c_customer_id 
-        FROM   customer_total_return ctr1, 
-               store, 
-               customer 
-        WHERE  ctr1.ctr_total_return > (SELECT Avg(ctr_total_return) * 1.2 
-                                        FROM   customer_total_return ctr2 
-                                        WHERE 
-               ctr1.ctr_store_sk = ctr2.ctr_store_sk) 
-               AND s_store_sk = ctr1.ctr_store_sk 
-               AND s_state = 'SD' 
+         GROUP  BY sr_customer_sk,
+                   sr_store_sk)
+SELECT *
+FROM   (SELECT c_customer_id
+        FROM   customer_total_return ctr1,
+               store,
+               customer
+        WHERE  ctr1.ctr_total_return > (SELECT Avg(ctr_total_return) * 1.2
+                                        FROM   customer_total_return ctr2
+                                        WHERE
+               ctr1.ctr_store_sk = ctr2.ctr_store_sk)
+               AND s_store_sk = ctr1.ctr_store_sk
+               AND s_state = 'SD'
                AND ctr1.ctr_customer_sk = c_customer_sk
                AND rownum <= 10000
-        ORDER  BY c_customer_id) 
-WHERE  rownum <= 100; 
+        ORDER  BY c_customer_id)
+WHERE  rownum <= 100;
--- src/sql/Runtime/TPC-DS/tpcds100/Query/query_56.sql
+++ src/sql/Runtime/TPC-DS/tpcds100/Query/query_56.sql
@@ -1,73 +1,73 @@
-WITH ss 
-     AS (SELECT i_item_id, 
-                Sum(ss_ext_sales_price) total_sales 
-         FROM   store_sales, 
-                date_dim, 
-                customer_address, 
-                item 
-         WHERE  i_item_id IN (SELECT i_item_id 
-                              FROM   item 
-                              WHERE  i_color IN ( 'khaki', 'powder', 'red' )) 
-                AND ss_item_sk = i_item_sk 
-                AND ss_sold_date_sk = d_date_sk 
-                AND d_year = 2002 
-                AND d_moy = 5 
-                AND ss_addr_sk = ca_address_sk 
+WITH ss
+     AS (SELECT i_item_id,
+                Sum(ss_ext_sales_price) total_sales
+         FROM   store_sales,
+                date_dim,
+                customer_address,
+                item
+         WHERE  i_item_id IN (SELECT i_item_id
+                              FROM   item
+                              WHERE  i_color IN ( 'khaki', 'powder', 'red' ))
+                AND ss_item_sk = i_item_sk
+                AND ss_sold_date_sk = d_date_sk
+                AND d_year = 2002
+                AND d_moy = 5
+                AND ss_addr_sk = ca_address_sk
                 AND ca_gmt_offset = -8
                 and rownum <= 10000
-         GROUP  BY i_item_id), 
-     cs 
-     AS (SELECT i_item_id, 
-                Sum(cs_ext_sales_price) total_sales 
-         FROM   catalog_sales, 
-                date_dim, 
-                customer_address, 
-                item 
-         WHERE  i_item_id IN (SELECT i_item_id 
-                              FROM   item 
-                              WHERE  i_color IN ( 'khaki', 'powder', 'red' )) 
-                AND cs_item_sk = i_item_sk 
-                AND cs_sold_date_sk = d_date_sk 
-                AND d_year = 2002 
-                AND d_moy = 5 
-                AND cs_bill_addr_sk = ca_address_sk 
+         GROUP  BY i_item_id),
+     cs
+     AS (SELECT i_item_id,
+                Sum(cs_ext_sales_price) total_sales
+         FROM   catalog_sales,
+                date_dim,
+                customer_address,
+                item
+         WHERE  i_item_id IN (SELECT i_item_id
+                              FROM   item
+                              WHERE  i_color IN ( 'khaki', 'powder', 'red' ))
+                AND cs_item_sk = i_item_sk
+                AND cs_sold_date_sk = d_date_sk
+                AND d_year = 2002
+                AND d_moy = 5
+                AND cs_bill_addr_sk = ca_address_sk
                 AND ca_gmt_offset = -8
                 and rownum <= 10000
-         GROUP  BY i_item_id), 
-     ws 
-     AS (SELECT i_item_id, 
-                Sum(ws_ext_sales_price) total_sales 
-         FROM   web_sales, 
-                date_dim, 
-                customer_address, 
-                item 
-         WHERE  i_item_id IN (SELECT i_item_id 
-                              FROM   item 
-                              WHERE  i_color IN ( 'khaki', 'powder', 'red' )) 
-                AND ws_item_sk = i_item_sk 
-                AND ws_sold_date_sk = d_date_sk 
-                AND d_year = 2002 
-                AND d_moy = 5 
-                AND ws_bill_addr_sk = ca_address_sk 
+         GROUP  BY i_item_id),
+     ws
+     AS (SELECT i_item_id,
+                Sum(ws_ext_sales_price) total_sales
+         FROM   web_sales,
+                date_dim,
+                customer_address,
+                item
+         WHERE  i_item_id IN (SELECT i_item_id
+                              FROM   item
+                              WHERE  i_color IN ( 'khaki', 'powder', 'red' ))
+                AND ws_item_sk = i_item_sk
+                AND ws_sold_date_sk = d_date_sk
+                AND d_year = 2002
+                AND d_moy = 5
+                AND ws_bill_addr_sk = ca_address_sk
                 AND ca_gmt_offset = -8
                 and rownum <= 10000
-         GROUP  BY i_item_id) 
-SELECT * 
-FROM   (SELECT i_item_id, 
-               Sum(total_sales) total_sales 
-        FROM   (SELECT * 
+         GROUP  BY i_item_id)
+SELECT *
+FROM   (SELECT i_item_id,
+               Sum(total_sales) total_sales
+        FROM   (SELECT *
                 FROM   ss
                 where rownum <= 10000
-                UNION ALL 
-                SELECT * 
+                UNION ALL
+                SELECT *
                 FROM   cs
                 where rownum <= 10000
-                UNION ALL 
-                SELECT * 
+                UNION ALL
+                SELECT *
                 FROM   ws
                 where rownum <= 10000) tmp1
         where rownum <= 10000
-        GROUP  BY i_item_id 
-        ORDER  BY total_sales, 
-                  i_item_id) 
-WHERE  rownum <= 100; 
+        GROUP  BY i_item_id
+        ORDER  BY total_sales,
+                  i_item_id)
+WHERE  rownum <= 100;
