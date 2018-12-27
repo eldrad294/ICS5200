ffe1c831cebf91f218677c36efa1a113ccbe9c59
Gabriel Sammut <gabriel.sammut@rs2.com>
2018-12-27 11:57:53 +0100

    Further TPC-DS Queries/DML adjustments

--- src/sql/Runtime/TPC-DS/tpcds100/Query/query_24.sql
+++ src/sql/Runtime/TPC-DS/tpcds100/Query/query_24.sql
@@ -17,6 +17,7 @@ from store_sales
     ,customer
     ,customer_address
 where ss_ticket_number = sr_ticket_number
+and c_customer_sk between 579 and 900
   and ss_item_sk = sr_item_sk
   and ss_customer_sk = c_customer_sk
   and ss_item_sk = i_item_sk
